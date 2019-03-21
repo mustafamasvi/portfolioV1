aa46df0e25934c8a791845a8ea7acc6d11a66cfb
Mustafa Masvi <mustafamasvi786@gmail.com>
2019-03-21 14:29:20 +0530

    Add github repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const config = require('./config/website')
 const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
 
 module.exports = {
+  pathPrefix: "/portfolio",
   /* General Information */
   siteMetadata: {
     siteUrl: config.siteUrl + pathPrefix,
