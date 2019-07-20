c20d64d4b06a5d3d9a3ddc1b7cb1f9a041a2df2f
Mustafa Masvi <mustafamasvi786@gmail.com>
2019-07-20 12:34:30 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,17 +26,17 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ae4d9fbc59ac9ad106c0.js"
+    "url": "webpack-runtime-756a0378dd0b1f0401e5.js"
   },
   {
-    "url": "app-b52204e8f157eb77394a.js"
+    "url": "app-8f57044d6f2ff7d1e5e0.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-aa0b1193cbc4b1fcbf28.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a1016be9464c8f0883e98e5efb667e27"
+    "revision": "ff7957b7c571c16023f7756e73c02158"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
